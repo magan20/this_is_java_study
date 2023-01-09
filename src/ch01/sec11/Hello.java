2df75c50fbd4cecf50169b3e326150e121803a5d
magan20 <audwls2160@naver.com>
2023-01-09 18:34:04 +0900

    FEAT: Review Part 01

---
+++ src/ch01/sec11/Hello.java
@@ -0,0 +1,13 @@
+package ch01.sec11;
+/**
+ * @author shin myeongjin
+ */
+/*
+장제목 : 1장 자바 시작하기
+작성일 : 2023.01.09
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java");
+    }
+}
---
+++ src/ch01/sec12/Calculator.java
@@ -0,0 +1,11 @@
+package ch01.sec12;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x;						//변수 x 선언
+        x = 1;						//변수 x에 1 값을 저장
+        int y = 2;					//변수 y를 선언하고 2 값을 저장
+        int result = x + y;			//변수 result 를 선언하고 변수 x와 y를 더한 값을 저장
+        System.out.println(result);	//콘솔에 변수의 값을 출력하는 println() 메소드 호출
+    }
+}
