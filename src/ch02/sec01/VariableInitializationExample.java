22c44df27b6c2111a649e892ac2c3323215fb8f6
magan20 <audwls2160@naver.com>
2023-01-09 20:50:44 +0900

    FEAT: ch02.sec01.VariableInitailizationExample

---
+++ src/ch02/sec01/VariableInitializationExample.java
@@ -0,0 +1,14 @@
+package ch02.sec01;
+
+public class VariableInitializationExample {
+    public static void main(String[] args) {
+        //변수 value 선언
+        int value;
+
+        //연산 결과를 변수 result 의 초기값으로 대입
+        //int result = value + 10;    //초기화되지 않은 변수를 연산식에 사용할 경우 컴파일 에러 발생
+
+        //변수 result 값을 읽고 콘솔에 출력
+        //System.out.println(result);
+    }
+}
